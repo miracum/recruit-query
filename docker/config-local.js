0b26931ca6d3fe578eb9310b8e630b6abc2f7aa2
Christian Gulden <christian.gulden@fau.de>
2019-08-08 18:40:08 +0200

    build: added development scaffolding for external services

---
+++ docker/config-local.js
@@ -0,0 +1,19 @@
+define([], function () {
+        var configLocal = {};
+
+        // clearing local storage otherwise source cache will obscure the override settings
+        localStorage.clear();
+
+        // WebAPI
+        configLocal.api = {
+                name: 'OHDSI',
+                url: 'http://localhost:8081/WebAPI/'
+        };
+
+        configLocal.cohortComparisonResultsEnabled = false;
+        configLocal.userAuthenticationEnabled = false;
+        configLocal.plpResultsEnabled = false;
+
+        return configLocal;
+});
+
