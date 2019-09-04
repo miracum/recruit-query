9485a1e13a3fb8d72c43b98180b77c02cc7abc7a
blrom1 <romina.blasini@informatik.med.uni-giessen.de>
2019-09-04 15:08:14 +0200

    Interface

---
+++ Query/OMOP_connector.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+namespace Query
+{
+    public class OMOP_connector
+    {
+        /// <summary>
+        /// Checks if database configurations specified in app.config are valid and database is of OMOP schema
+        /// </summary>
+        /// <returns> true if valid, false if not </returns>
+        public static boolean checkConnection()
+        {
+
+        }
+
+
+
+
+        /// <summary>
+        /// Requests all Patient IDs connected to the given cohort
+        /// </summary>
+        /// <param string="cohortId"> Id of the requested cohort </param>
+        /// <returns> ArrayList of Ids</returns>
+        public static ArrayList<string> getIdsFromCohort(string cohortId)
+        {
+            return new ArrayList<string>();
+        }
+
+
+    }
+}
