724f8f68581e293cbb1180ef439d38fdcea3cce0
Christian Gulden <christian.gulden@fau.de>
2019-09-04 19:37:22 +0200

    style: applied stylecop rules

---
+++ Query/OmopConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Query
+{
+    public class OmopConnector
+    {
+        public static bool CheckConnection()
+        {
+            return true;
+        }
+
+        public static ArrayList GetIdsFromCohort(string cohortId)
+        {
+            return new ArrayList();
+        }
+    }
+}
