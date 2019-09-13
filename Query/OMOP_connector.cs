eabb40701018c29597673b7dfeaa176229221b0e
blrom1 <romina.blasini@informatik.med.uni-giessen.de>
2019-09-13 09:10:16 +0200

    fix: correction on OMOP_connector documentation

--- Query/OMOP_connector.cs
+++ Query/OMOP_connector.cs
@@ -2,33 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 namespace Query
 {
+    /// <summary>
+    /// TODO: Beschreibung.
+    /// </summary>
     public class OMOP_connector
     {
         /// <summary>
-        /// Checks if database configurations specified in app.config are valid and database is of OMOP schema
+        /// Checks if database configurations specified in app.config are valid and database is of OMOP schema.
         /// </summary>
-        /// <returns> true if valid, false if not </returns>
-        public static bool checkConnection()
+        /// <returns> true if valid, false if not.</returns>
+        public static bool CheckConnection()
         {
-
+            return true;
         }
 
-
-
-
         /// <summary>
-        /// Requests all Patient IDs connected to the given cohort
+        /// Requests all Patient IDs connected to the given cohort.
         /// </summary>
-        /// <param string="cohortId"> Id of the requested cohort </param>
-        /// <returns> ArrayList of Ids</returns>
-        public static ArrayList getIdsFromCohort(string cohortId)
+        /// <param name="cohortId"> Id of the requested cohort.</param>
+        /// <returns> ArrayList of Ids.</returns>
+        public static ArrayList GetIdsFromCohort(string cohortId)
         {
             return new ArrayList();
         }
-
-
     }
 }
