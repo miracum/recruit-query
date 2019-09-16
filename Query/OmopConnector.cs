7ad03aa2420a35e8030ca0dec759cf4994ec7a75
blrom1 <romina.blasini@informatik.med.uni-giessen.de>
2019-09-16 10:39:16 +0200

    feat: implementing OmopConnector

--- Query/OmopConnector.cs
+++ Query/OmopConnector.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Query.Models.DB;
 
 namespace Query
 {
     /// <inheritdoc />
     public class OmopConnector : IOmopDatabaseClient
     {
+        private OHDSIContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OmopConnector"/> class.
+        /// Creates new Object of OmopConnector.
+        /// </summary>
+        public OmopConnector()
+        {
+            this.context = new OHDSIContext();
+        }
+
         /// <summary>
         /// Checks if database configurations specified in app.config are valid and database is of OMOP schema.
         /// </summary>
@@ -16,10 +31,38 @@ namespace Query
             return true;
         }
 
-        /// <inheritdoc />
-        public Task<List<string>> GetIdsFromCohort(string id)
+        /// <summary>
+        /// Requests all Patient IDs connected to the given cohort.
+        /// </summary>
+        /// <param name="cohortId"> Id of the requested cohort.</param>
+        /// <returns> ArrayList of Ids.</returns>
+        public Task<List<string>> GetIdsFromCohort(string cohortId)
         {
+            var ids = context.Cohort.Where(b => b.SubjectId.Equals(cohortId));
+
+            List<Cohort> liste = ids.ToList();
+
+            // Task<List<string>> task = new Task<List<string>>(ids);
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Requests all Patient IDs connected to the given cohort.
+        /// </summary>
+        /// <param name="cohortId"> Id of the requested cohort.</param>
+        /// <returns> ArrayList of Ids.</returns>
+        public List<long> GetIdListFromCohort(string cohortId)
+        {
+            var ids = context.Cohort.Where(b => b.SubjectId.Equals(cohortId));
+
+            List<Cohort> liste = ids.ToList();
+            List<long> idList = new List<long>();
+            foreach (Cohort cohort in liste)
+            {
+                idList.Add(cohort.SubjectId);
+            }
+
+            return idList;
         }
     }
 }
--- Query/Program.cs
+++ Query/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 #pragma warning disable CS1591, SA1600
@@ -10,6 +11,7 @@ namespace Query
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Test");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
