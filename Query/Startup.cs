be5560c9c2b9df57a09156bc2102dc8ee835778d
Christian Gulden <christian.gulden@fau.de>
2019-09-22 23:08:47 +0200

    style: removed trailing whitespace

--- Query/Startup.cs
+++ Query/Startup.cs
@@ -23,7 +23,7 @@ namespace Query
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {           
+        {
             services.AddDbContext<OmopContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("OmopDatabase")));
 
