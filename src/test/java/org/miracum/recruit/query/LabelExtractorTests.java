e9d52d468d1f43f5744b24909399a8cff3d7ab36
Christian Gulden <christian.gulden@fau.de>
2020-09-12 02:10:31 +0200

    test(unit): fixed failing tests due to dependency updates

--- src/test/java/org/miracum/recruit/query/LabelExtractorTests.java
+++ src/test/java/org/miracum/recruit/query/LabelExtractorTests.java
@@ -1,39 +1,36 @@
 package org.miracum.recruit.query;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-public class LabelExtractorTests {
+class LabelExtractorTests {
 
     private final LabelExtractor sut = new LabelExtractor();
 
     @Test
-    public void extract_WithEmptyString_returnsEmptySet() {
+    void extract_WithEmptyString_returnsEmptySet() {
         var result = sut.extract("");
 
         assertThat(result).isEmpty();
     }
 
     @Test
-    public void extract_WithNullString_returnsDisctinct() {
+    void extract_WithNullString_returnsDisctinct() {
         var result = sut.extract(null);
 
         assertThat(result).isEmpty();
     }
 
     @Test
-    public void extract_WithDuplicateLabels_returnsDisctinct() {
+    void extract_WithDuplicateLabels_returnsDisctinct() {
         var result = sut.extract("[a] abc [b] [a]");
 
         assertThat(result).contains("a", "b");
     }
 
     @Test
-    public void extract_WithoutLabels_returnsEmptySet() {
+    void extract_WithoutLabels_returnsEmptySet() {
         var result = sut.extract("hello world ]");
 
         assertThat(result).isEmpty();
