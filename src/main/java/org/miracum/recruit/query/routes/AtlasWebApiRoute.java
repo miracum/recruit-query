f3abf8202ed9d38085d34486e526fa9ca9c03246
Christian Gulden <christian.gulden@fau.de>
2021-04-30 16:19:43 +0200

    refactor: use structured logging

--- src/main/java/org/miracum/recruit/query/routes/AtlasWebApiRoute.java
+++ src/main/java/org/miracum/recruit/query/routes/AtlasWebApiRoute.java
@@ -1,5 +1,7 @@
 package org.miracum.recruit.query.routes;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import com.google.common.collect.Sets;
 import java.util.HashSet;
 import java.util.Set;
@@ -45,87 +47,86 @@ public class AtlasWebApiRoute extends RouteBuilder {
   public void configure() {
     // general error handler
     errorHandler(
-        defaultErrorHandler()
-            .maximumRedeliveries(5)
-            .redeliveryDelay(5000)
-            .retryAttemptedLogLevel(LoggingLevel.WARN));
+      defaultErrorHandler()
+        .maximumRedeliveries(5)
+        .redeliveryDelay(5000)
+        .retryAttemptedLogLevel(LoggingLevel.WARN));
 
     // in case of a http exception then retry at most 3 times
     onException(HttpOperationFailedException.class)
-        .maximumRedeliveries(2)
-        .handled(true)
-        .delay(5000)
-        .log(
-            LoggingLevel.WARN,
-            LOG,
-            "HTTP error during request processing. Failing after retrying.");
+      .maximumRedeliveries(2)
+      .handled(true)
+      .delay(5000)
+      .log(
+        LoggingLevel.WARN,
+        LOG,
+        "HTTP error during request processing. Failing after retrying.");
 
     // when running all cohorts
     from(GET_COHORT_DEFINITIONS)
-        .removeHeaders("CamelHttp*")
-        .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-        .to(baseUrl + "/cohortdefinition")
-        .convertBodyTo(String.class)
-        .log(LoggingLevel.DEBUG, LOG, "response from webapi: ${body}")
-        .split()
-        .jsonpathWriteAsString("$[*]") // foreach cohort.
-        // https://camel.apache.org/components/latest/jsonpath-component.html
-        .unmarshal()
-        .json(JsonLibrary.Jackson, CohortDefinition.class) // Convert from json to
-        // CohortDefinition-Object
-        .to(RUN_COHORT_GENERATION)
-        .end();
+      .removeHeaders("CamelHttp*")
+      .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+      .to(baseUrl + "/cohortdefinition")
+      .convertBodyTo(String.class)
+      .log(LoggingLevel.DEBUG, LOG, "response from webapi: ${body}")
+      .split()
+      .jsonpathWriteAsString("$[*]") // foreach cohort.
+      // https://camel.apache.org/components/latest/jsonpath-component.html
+      .unmarshal()
+      .json(JsonLibrary.Jackson, CohortDefinition.class) // Convert from json to
+      // CohortDefinition-Object
+      .to(RUN_COHORT_GENERATION)
+      .end();
 
     // when running just one cohort
     from(GET_COHORT_DEFINITION)
-        .removeHeaders("CamelHttp*")
-        .log("processing cohort: ${body}")
-        .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-        .toD(baseUrl + "/cohortdefinition/${body}")
-        .convertBodyTo(String.class)
-        .unmarshal()
-        .json(JsonLibrary.Jackson, CohortDefinition.class) // Convert
-        // from
-        // json
-        // to
-        // CohortDefinition-Object
-        .log(LoggingLevel.DEBUG, LOG, "[cohort ${body.id}] response from webapi: ${body}")
-        .process(
-            ex -> {
-              var body = (CohortDefinition) ex.getIn().getBody();
-              ex.getIn().setBody(body);
-            })
-        .to(RUN_COHORT_GENERATION);
+      .removeHeaders("CamelHttp*")
+      .log("processing cohort: ${body}")
+      .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+      .toD(baseUrl + "/cohortdefinition/${body}")
+      .convertBodyTo(String.class)
+      .unmarshal()
+      // Convert from json to CohortDefinition-Object
+      .json(JsonLibrary.Jackson, CohortDefinition.class)
+      .log(LoggingLevel.DEBUG, LOG, "[cohort ${body.id}] response from webapi: ${body}")
+      .process(
+        ex -> {
+          var body = (CohortDefinition) ex.getIn().getBody();
+          ex.getIn().setBody(body);
+        })
+      .to(RUN_COHORT_GENERATION);
 
     // generate a cohort
     from(RUN_COHORT_GENERATION)
-        .removeHeaders("CamelHttp*")
-        .log("[Cohort ${body.id}] processing cohort: ${body}")
-        .filter()
-        .method(this, "isMatchingCohort")
-        .log(LoggingLevel.INFO, LOG, "[Cohort ${body.id}] cohort matches the selector labels")
-        .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-        // needed otherwise ConvertException
-        .setHeader("cohort", body())
-        .setBody(constant(null))
-        .toD(baseUrl + "/cohortdefinition/${header.cohort.id}/generate/" + dataSourceName)
-        .setHeader(HEADER_GENERATION_STATUS, constant("PENDING"))
-        // Check Status of generation and loop while still running
-        .loopDoWhile(header(HEADER_GENERATION_STATUS).regex("PENDING|RUNNING"))
-        .toD(baseUrl + "/cohortdefinition/${header.cohort.id}/info")
-        .convertBodyTo(String.class)
-        .setHeader(HEADER_GENERATION_STATUS, jsonpath("$.[0].status"))
-        .log("[Cohort ${header.cohort.id}] current status: ${header.generationStatus}")
-        .delay(simple("${properties:atlas.cohortStatusCheckBackoffTime}"))
-        .end()
-        .choice()
-        .when(header(HEADER_GENERATION_STATUS).isEqualTo("COMPLETE"))
-        .setBody(header("cohort"))
-        .to(Router.DONE_COHORT_GENERATION);
+      .removeHeaders("CamelHttp*")
+      .log("processing cohort: ${body}")
+      .filter()
+      .method(this, "isMatchingCohort")
+      .log( "cohort=${body.id} matches the selector labels")
+      .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+      // needed otherwise ConvertException
+      .setHeader("cohort", body())
+      .setBody(constant(null))
+      .toD(baseUrl + "/cohortdefinition/${header.cohort.id}/generate/" + dataSourceName)
+      .setHeader(HEADER_GENERATION_STATUS, constant("PENDING"))
+      // Check Status of generation and loop while still running
+      .loopDoWhile(header(HEADER_GENERATION_STATUS).regex("PENDING|RUNNING"))
+      .toD(baseUrl + "/cohortdefinition/${header.cohort.id}/info")
+      .convertBodyTo(String.class)
+      .setHeader(HEADER_GENERATION_STATUS, jsonpath("$.[0].status"))
+      .log(LoggingLevel.INFO, log,
+        "cohort=${header.cohort.id} current status=${header.generationStatus}")
+      .delay(simple("${properties:atlas.cohortStatusCheckBackoffTime}"))
+      .end()
+      .choice()
+      .when(header(HEADER_GENERATION_STATUS).isEqualTo("COMPLETE"))
+      .setBody(header("cohort"))
+      .to(Router.DONE_COHORT_GENERATION);
   }
 
   public boolean isMatchingCohort(@Body CohortDefinition definition) {
-    log.info("[Cohort {}] Checking against match labels {}.", definition.getId(), matchLabels);
+    log.info("Checking if {} matches labels {}.", kv("cohort", definition.getId()),
+      kv("matchLabels", matchLabels));
     if (matchLabels.isEmpty()) {
       // if no match labels are specified, simply accept all cohorts
       return true;
