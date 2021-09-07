afec745d572889934a3e475e5c0cc429b36be410
Christian Gulden <christian.gulden@fau.de>
2021-09-07 14:42:00 +0200

    fix: use a LEFT-join to make sure to include visit occurrences without any care site

--- src/main/java/org/miracum/recruit/query/repositories/VisitDetailRepository.java
+++ src/main/java/org/miracum/recruit/query/repositories/VisitDetailRepository.java
@@ -9,7 +9,7 @@ public interface VisitDetailRepository extends PagingAndSortingRepository<VisitD
   @Query(
       "SELECT visit_detail.*, care_site.* "
           + "FROM visit_detail "
-          + "         JOIN care_site ON visit_detail.care_site_id = care_site.care_site_id "
+          + "  LEFT JOIN care_site ON visit_detail.care_site_id = care_site.care_site_id "
           + "WHERE visit_detail.visit_occurrence_id = :visitOccurrenceId "
           + "ORDER BY visit_detail_start_date DESC "
           + "LIMIT 5")
