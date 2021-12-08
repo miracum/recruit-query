01026a1d934491f07d9ea10cf95ce0cafae51e22
Christian Gulden <christian.gulden@fau.de>
2021-12-08 22:18:50 +0100

    refactor: use new Java 17 multiline strings

--- src/main/java/org/miracum/recruit/query/repositories/VisitDetailRepository.java
+++ src/main/java/org/miracum/recruit/query/repositories/VisitDetailRepository.java
@@ -8,14 +8,16 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface VisitDetailRepository extends PagingAndSortingRepository<VisitDetail, Long> {
 
   @Query(
-      "SELECT visit_detail.*, care_site.care_site_name "
-          + "FROM visit_detail "
-          + "  LEFT JOIN care_site ON visit_detail.care_site_id = care_site.care_site_id "
-          + "WHERE visit_detail.visit_occurrence_id = :visitOccurrenceId "
-          + "AND visit_detail.visit_detail_source_value IS NOT NULL "
-          + "ORDER BY visit_detail_start_date DESC "
-          + "LIMIT 5")
-  Set<VisitDetail>
-      findTop5ByVisitOccurrenceIdAndVisitDetailSourceValueIsNotNullOrderByVisitDetailStartDateDesc(
-          Long visitOccurrenceId);
+      """
+          SELECT visit_detail.*, care_site.care_site_name
+          FROM visit_detail
+          LEFT JOIN care_site ON visit_detail.care_site_id = care_site.care_site_id
+          WHERE visit_detail.visit_occurrence_id = :visitOccurrenceId
+          -- only fetch visit_details which either have a care_site or a source_value set
+          AND visit_detail.visit_detail_source_value IS NOT NULL OR visit_detail.care_site_id IS NOT NULL
+          ORDER BY visit_detail_start_date DESC
+          LIMIT 5
+      """)
+  Set<VisitDetail> findTop5ByVisitOccurrenceIdOrderByVisitDetailStartDateDesc(
+      Long visitOccurrenceId);
 }
